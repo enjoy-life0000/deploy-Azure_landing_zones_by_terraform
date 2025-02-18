07e0a4082b08a901b5113fcb4385f21e19c03a2d
enjoy-life0000 <enjoylife.follower@gmail.com>
2025-02-18 18:16:08 -0500

    - Call the landing zone module

--- main.tf
+++ main.tf
@@ -10,3 +10,22 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+data "azurerm_client_config" "core" {}
+
+module "enterprise_scale" {
+  source  = "Azure/caf-enterprise-scale/azurerm"
+  version = "~> 5.2"
+
+  default_location = "West US"
+
+  providers = {
+    azurerm              = azurerm
+    azurerm.connectivity = azurerm
+    azurerm.management   = azurerm
+  }
+
+  root_parent_id = data.azurerm_client_config.core.tenant_id
+  root_id        = "myorg"
+  root_name      = "My Organization"
+}
